d22e09bc68e5ac92bea1061900253d77140429f6
Oscar Romero Doblas <oscar.romero@students.salle.url.edu>
2022-04-07 18:57:42 +0200

    Login Layout (DONE)

--- app/src/main/java/com/androidpprog2/openevents/MainActivity.java
+++ app/src/main/java/com/androidpprog2/openevents/MainActivity.java
@@ -2,9 +2,13 @@ package com.androidpprog2.openevents;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView singUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +16,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
 
+
+        singUp = (TextView) findViewById(R.id.SingUp);
+        singUp.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                //Linkear pag Registro -> VICTOR
+                Intent i = new Intent(MainActivity.this, SplashScreenActivity.class);
+                startActivity(i);
+            }
+        });
     }
-}
+    }
